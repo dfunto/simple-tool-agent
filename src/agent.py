c656aab873d951329f0144625a8c8ac3ec36e934
dfunto <dadutra2@gmail.com>
2026-02-01 16:43:41 +0100

    very dumb agent

--- src/agent.py
+++ src/agent.py
@@ -1,57 +1,66 @@
+import json
+from pathlib import Path
+
 import requests
-import textwrap
 
 from models import AgentObservation
 from logs import LoggingMixin
-from src.models import AgentPlan
+from models import AgentPlan, AgentTool
+from models import AgentResult
 
 
 class Agent(LoggingMixin):
 
-    def observe(self) -> AgentObservation:
-        print("How can I help you today? (Hit CTRL + C to exit)")
-        prompt = input()
-        return AgentObservation(prompt=prompt)
-
-    def plan(self, observation: AgentObservation) -> AgentPlan:
-        plan_prompt = textwrap.dedent(f"""
-        You are a tool-selection engine.
-        Rules:
-            - Respond with VALID JSON ONLY
-            - No explanations
-            - No markdown
-            - No extra text
-
-        User input: "{observation.prompt}"
-        Available tools: "{','.join(observation.tools)}"
-        
-        Task: Decide which tool to use and what input to provide.
-        Output Example: {{"tool": "selected tool", "input": "user input for the tool"}}
-        """)
-        self.log.info(f"Plan Prompt:{plan_prompt}")
+    @staticmethod
+    def _query_llm(prompt: str) -> str:
         response = requests.post(
             url="http://llm:11434/api/generate",
             json={
                 "model": "llama3.2:3b",
-                "prompt": plan_prompt,
+                "prompt": prompt,
                 "format": "json",
                 "stream": False
-            },
+            }
         )
         response.raise_for_status()
-        plan_output = response.json().get("response")
-        self.log.info(f"Plan Output:\n{plan_output}")
-        return AgentPlan(**plan_output)
+        return response.json().get("response")
 
-    def act(self, plan: AgentPlan):
-        print("Acting")
+    def observe(self) -> AgentObservation:
+        print("How can I help you today? (Hit CTRL + C to exit)")
+        prompt = input()
+        self.log.debug(f"Prompt received {prompt}")
+        return AgentObservation(prompt=prompt)
 
-    def reflect(self):
-        print("Reflecting")
+    def plan(self, observation: AgentObservation) -> AgentPlan:
+        self.log.debug(f"Plan Prompt:{observation.plan_prompt}")
+        output = self._query_llm(prompt=observation.plan_prompt)
+        self.log.info(f"Plan Output:\n{output}")
+        return AgentPlan(**json.loads(output))
+
+    def act(self, plan: AgentPlan) -> AgentResult:
+        if plan.tool == AgentTool.CALCULATOR:
+            self.log.info(f"Calculating {plan.input}")
+            output = str(eval(plan.input))
+        elif plan.tool == AgentTool.FILE_READER:
+            self.log.info(f"Reading file {plan.input}")
+            file = Path("./input") / plan.input
+            lines = open(file).readlines()
+            output = "\n".join(lines)
+        else:
+            raise NotImplemented(f"Tool {plan.tool} not implemented")
+
+        result = AgentResult(input=plan.input, output=output)
+        return result
+
+    def reflect(self, result: AgentResult) -> bool:
+        self.log.debug(f"Result: {result}")
+        print(f"Output: {result.output}")
+        return True
 
     def run(self):
         while True:
             observation = self.observe()
             plan = self.plan(observation=observation)
-            self.act()
-            self.reflect()
+            result = self.act(plan=plan)
+            if not self.reflect(result=result):
+                raise ValueError("Failed to achieve result")
--- src/models.py
+++ src/models.py
@@ -1,11 +1,58 @@
+import textwrap
+from enum import Enum
 from pydantic import Field, BaseModel
+
+
+class AgentTool(Enum):
+    CALCULATOR = "calculator"
+    FILE_READER = "file_reader"
 
 
 class AgentObservation(BaseModel):
     prompt: str
-    tools: list[str] = Field(default_factory=lambda: ["calculator", "file_reader"])
+    tools: list[AgentTool] = Field(default_factory=lambda: list(AgentTool))
+
+    @property
+    def plan_prompt(self) -> str:
+        tools = ','.join([tool.value for tool in self.tools])
+        return textwrap.dedent(f"""
+        You are a tool-selection engine.
+        Rules:
+            - Respond with VALID JSON ONLY
+            - No explanations
+            - No markdown
+            - No extra text
+
+        User input: "{self.prompt}"
+        Available tools: "{tools}"
+        
+        Task: Decide which tool to use and what input to provide.
+        Output Example: {{"tool": "selected tool", "input": "user input for the tool"}}
+        """)
 
 
 class AgentPlan(BaseModel):
-    tool: str
+    tool: AgentTool
     input: str
+
+
+class AgentResult(BaseModel):
+    input: str
+    output: str
+
+    @property
+    def reflect_prompt(self) -> str:
+        return textwrap.dedent(f"""
+        Is the 
+        Rules:
+            - Respond with VALID JSON ONLY
+            - No explanations
+            - No markdown
+            - No extra text
+
+        User input: "{self.prompt}"
+        Available tools: "{tools}"
+
+        Task: Decide which tool to use and what input to provide.
+        Output Example: {{"tool": "selected tool", "input": "user input for the tool"}}
+        """)
